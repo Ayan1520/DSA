36eca6b4996c1c240c49f746ba8b267b8ead3646
Ayan Pandit <ayanpandit1511@gmail.com>
2026-04-14 21:05:57 +0530

    Delete dsa in C++ directory

--- dsa in C++/set_ithbitornot.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int x = 13;
-    cout<<(x&(1<<2));
-    return 0;
-}
--- dsa in C++/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-vector<int> vis(7, -1);
-    Graph g(7);
-    g.addEdge(1, 2);
-    g.addEdge(2, 3);
-    g.addEdge(3, 4);
-    g.addEdge(4, 6);
-    g.addEdge(2, 4);
-    g.toposort();
-    return 0;
